199c2ed6cf87ca832be21975109c29b6219b45d1
acheron <acheron@pop-os.localdomain>
2020-04-13 18:57:12 +0200

    Datetime cast

--- http-pinger-backend/app/Models/Server.php
+++ http-pinger-backend/app/Models/Server.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Server extends Model
 {
     protected $fillable = ["name", "ip",];
+
+    protected $casts = [
+        'created_at'  => 'date:Y-m-d H:i:s',
+        'updated_at'  => 'date:Y-m-d H:i:s',
+    ];
 }
